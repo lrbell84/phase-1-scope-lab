ddbed41f835138bd19c2c90ba80ebb399faf821b
Lydia Bell <lrbell84@gmail.com>
2021-07-11 03:14:24 +0000

    commit tree 5b463b946672a9902f32b200169ef9080247d129

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var customerName = 'bob'
+const leastFavoriteCustomer = 'random value'
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase()
+    
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob'
+}
+
 // Write your solution in this file!
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'ricky'
+}
